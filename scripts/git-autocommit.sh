664748e8f36d00eb2d3f68f09ba0336949989576
blackworkers <gpadmin@gpdb-sandbox.localdomain>
2018-08-21 06:58:46 -0400

    Github Auto-commit via Blackworkers Cron 2018-08-21_06:58:46

--- scripts/git-autocommit.sh
+++ scripts/git-autocommit.sh
@@ -1,5 +1,5 @@
 DATE=`date +%F_%T`
 cd ~/blackworkers/blackworkersapp/
 git add -A
-git commit -m "Github Auto-commit via Pivotal Cron $DATE"
+git commit -m "Github Auto-commit via Blackworkers Cron $DATE"
 git push origin master
---
+++ scripts/setup_python.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -e
+
+# Setup correct repo
+sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
+
+# Updated Yum
+sudo yum -y update
+
+# Install Python 3
+sudo yum install -y python36u python36u-libs python36u-devel python36u-pip
+
+# Check to make sure python is installed
+python3.6 -V
+
+# Make Python 3.6 the default for the DSACC account interactive shell
+echo 'alias python="python3.6"' >> /home/gpadmin/.bash_profile
+
+# Make Python 3.6 the default for the DSACC account non-interactive shell
+echo 'alias python="python3.6"' >> /home/gpadmin/.bashrc
+
+# Make Pip 3.6 the default for the DSACC account interactive shell
+echo 'alias pip="pip3.6"' >> /home/gpadmin/.bash_profile
+
+# Make Pip 3.6 the default for the DSACC account non-interactive shell
+echo 'alias pip="pip3.6"' >> /home/gpadmin/.bashrc
+
+# Source .bashrc
+source /home/gpadmin/.bashrc
+
+# Upgrade Pip
+sudo pip3.6 install --upgrade pip
+
+# Install Pulp
+sudo pip3.6 install pulp
+
+# Install scrapy
+sudo pip3.6 install scrapy
