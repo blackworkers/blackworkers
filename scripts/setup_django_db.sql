8d835d3dc1e6657d52fcabd88d2a2c2ec2ce666f
antoniopetrole <apetrole@pivotal.io>
2018-09-13 11:11:05 -0400

    Added Django DB Setup Script

---
+++ scripts/setup_django_db.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE dj;
+
+CREATE USER django WITH PASSWORD 'django';
+
+ALTER ROLE django SET client_encoding TO 'utf8';
+ALTER ROLE django SET default_transaction_isolation TO 'read committed';
+ALTER ROLE django SET timezone TO 'UTC';
+
+GRANT ALL PRIVILEGES ON DATABASE dj TO django;
