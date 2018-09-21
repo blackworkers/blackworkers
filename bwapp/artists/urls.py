d489b2423cfcb4b390603a88a6305e8859d2c1fe
antoniopetrole <apetrole@pivotal.io>
2018-09-21 11:22:23 -0400

    Fixed CSS not being founds

--- bwapp/artists/urls.py
+++ bwapp/artists/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from artists import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # A view function that will be called if the URL pattern is detected: views.index,  which is the function named index() in the views.py file.
@@ -9,3 +10,5 @@ urlpatterns = [
     path('artists/<int:pk>', views.ArtistDetailView.as_view(), name='artist-detail'),
     path('us_artist_list/', views.USArtistListView.as_view(), name='us-artist-list'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
